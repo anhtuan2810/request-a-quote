641d0592bf8c11a6f21bde165cf1768368327831
anhtuan2810 <jos.anhtuan99@gmail.com>
2023-04-03 16:48:43 +0700

    update

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const getProductTitle = document.querySelector('[data-hook=product-title]');
-console.log(getProductTitle);
+const newElement = document.createElement('button');
+newElement.innerHTML = 'Request A Quote'
+newElement.setAttribute("class", "request-a-quote-button")
+getProductTitle.insertAdjacentElement('afterend', newElement);
+const style = document.createElement('style');
+style.innerHTML = `
+      .request-a-quote-button {
+        background-color: black;
+        color:white;
+      }
+    `;
+document.head.appendChild(style);
