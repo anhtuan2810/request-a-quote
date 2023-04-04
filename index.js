a274be5d2ea3ececc883e6b0836016f3c51e9901
anhtuan2810 <jos.anhtuan99@gmail.com>
2023-04-04 08:47:56 +0700

    update

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const getProductTitle = document.querySelector('[data-hook=product-title]');
-const newElement = document.createElement('button');
-newElement.textContent = 'Request A Quote'
-newElement.setAttribute("class", "request-a-quote-button")
-insertAfter(newElement, getProductTitle)
-const style = document.createElement('style');
-style.innerHTML = `
-      .request-a-quote-button {
-        background-color: black;
-        color:white;
-        padding: 5px 10px;
-      }
-    `;
-document.head.appendChild(style);
+setTimeout(() => {
+    settingQuoteButton()
+    setStyle()
+}, 1500)
 
-function insertAfter(newNode, existingNode) {
+const settingQuoteButton = () => {
+    const getProductTitle = document.querySelector('[data-hook=product-title]');
+    const newElement = document.createElement('button');
+    newElement.textContent = 'Request A Quote'
+    newElement.setAttribute("class", "request-a-quote-button")
+    insertAfter(newElement, getProductTitle)
+}
+
+
+const insertAfter = (newNode, existingNode) => {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+const setStyle = () => {
+    const style = document.createElement('style');
+    style.innerHTML = `
+          .request-a-quote-button {
+            background-color: black;
+            color:white;
+            padding: 5px 10px;
+          }
+        `;
+    document.head.appendChild(style);
 }
