73fbf621ce1f637a6fb8fb4e3cd0d3decce3bcab
anhtuan2810 <jos.anhtuan99@gmail.com>
2023-04-21 11:07:38 +0700

    add

---
+++ index.php
@@ -0,0 +1,250 @@
+<?php
+return  [
+    "product" => [
+      "id" => "1ee3c6c6-e3ff-f0d7-6609-9c1d151058d4",
+      "name" => "Now Product",
+      "slug" => "now-product",
+      "visible" => true,
+      "productType" => "physical",
+      "description" => "I'm a product description. I'm a great place to add more details about your product such as sizing, material, care instructions and cleaning instructions.",
+      "weightRange" => [
+        "minValue" => 0.0,
+        "maxValue" => 0.0,
+      ],
+      "stock" =>  [
+        "trackInventory" => true,
+        "quantity" => 70,
+        "inStock" => true,
+        "inventoryStatus" => "IN_STOCK",
+      ],
+      "price" =>  [
+        "currency" => "VND",
+        "price" => 13.0,
+        "discountedPrice" => 13.0,
+        "formatted" => [
+          "price" => "13₫",
+          "discountedPrice" => "13₫",
+        ]
+        ],
+      "priceData" =>  [
+        "currency" => "VND",
+        "price" => 13.0,
+        "discountedPrice" => 13.0,
+        "formatted" => [
+          "price" => "13₫",
+          "discountedPrice" => "13₫",
+        ]
+        ],
+      "convertedPriceData" =>  [
+        "currency" => "VND",
+        "price" => 13.0,
+        "discountedPrice" => 13.0,
+        "formatted" =>  [
+          "price" => "13₫",
+          "discountedPrice" => "13₫",
+        ]
+        ],
+      "priceRange" =>  [
+        "minValue" => 2011.0,
+        "maxValue" => 2072.0,
+      ],
+      "costRange" =>  [
+        "minValue" => 1000.0,
+        "maxValue" => 1000.0,
+      ],
+      "additionalInfoSections" =>  [
+        [
+          "title" => "PRODUCT INFO",
+          "description" => "I'm a product detail. I'm a great place to add more information about your product such as sizing, material, care and cleaning instructions. This is also a great space to write what makes this product special and how your customers can benefit from this item."
+        ],
+         [
+          "title" => "RETURN & REFUND POLICY",
+          "description" => "I’m a Return and Refund policy. I’m a great place to let your customers know what to do in case they are dissatisfied with their purchase. Having a straightforward refund or exchange policy is a great way to build trust and reassure your customers that they can buy with confidence."
+        ],
+        [
+          "title" => "SHIPPING INFO",
+          "description" => "I'm a shipping policy. I'm a great place to add more information about your shipping methods, packaging and cost. Providing straightforward information about your shipping policy is a great way to build trust and reassure your customers that they can buy from you with confidence."
+        ]
+        ],
+      "ribbons" =>  [
+       [
+          "text" => "Top Sellers"
+        ]
+       ],
+      "media" =>  [
+        "mainMedia" => [
+          "thumbnail" =>  [
+            "url" => "https://static.wixstatic.com/media/45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg/v1/fit/w_50,h_50,q_90/file.jpg",
+            "width" => 50,
+            "height" => 50,
+          ],
+          "mediaType" => "image",
+          "title" => "45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg",
+          "image" =>  [
+            "url" => "https://static.wixstatic.com/media/45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg/v1/fit/w_750,h_750,q_90/file.jpg",
+            "width" => 750,
+            "height" => 750,
+          ],
+          "id" => "45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg"
+        ],
+        "items" => [
+           [
+            "thumbnail" =>  [
+              "url" => "https://static.wixstatic.com/media/45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg/v1/fit/w_50,h_50,q_90/file.jpg",
+              "width" => 50,
+              "height" => 50,
+            ],
+            "mediaType" => "image",
+            "title" => "45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg",
+            "image" => [
+              "url" => "https://static.wixstatic.com/media/45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg/v1/fit/w_750,h_750,q_90/file.jpg",
+              "width" => 750,
+              "height" => 750
+            ],
+            "id" => "45d10e_dd5d9543998c4c0f9ecf120fac20ffb0~mv2.jpg",
+        ],
+           [
+            "thumbnail" => [
+              "url" => "https://static.wixstatic.com/media/45d10e_35c84fb1d48540f1886b2ceb7a342c37~mv2_d_3500_1968_s_2.jpg/v1/fit/w_50,h_50,q_90/file.jpg",
+              "width" => 50,
+              "height" => 50,
+            ],
+            "mediaType" => "image",
+            "title" => "45d10e_35c84fb1d48540f1886b2ceb7a342c37~mv2_d_3500_1968_s_2.jpg",
+            "image" =>  [
+              "url" => "https://static.wixstatic.com/media/45d10e_35c84fb1d48540f1886b2ceb7a342c37~mv2_d_3500_1968_s_2.jpg/v1/fit/w_3500,h_1968,q_90/file.jpg",
+              "width" => 3500,
+              "height" => 1968,
+            ],
+            "id" => "45d10e_35c84fb1d48540f1886b2ceb7a342c37~mv2_d_3500_1968_s_2.jpg",
+        ],
+        [
+            "thumbnail" =>  [
+              "url" => "https://static.wixstatic.com/media/45d10e_d6445cbc0df2428d9348c3a7639dbed8~mv2.jpg/v1/fit/w_50,h_50,q_90/file.jpg",
+              "width" => 50,
+              "height" => 50,
+            ],
+            "mediaType" => "image",
+            "title" => "45d10e_d6445cbc0df2428d9348c3a7639dbed8~mv2.jpg",
+            "image" =>[
+              "url" => "https://static.wixstatic.com/media/45d10e_d6445cbc0df2428d9348c3a7639dbed8~mv2.jpg/v1/fit/w_750,h_750,q_90/file.jpg",
+              "width" => 750,
+              "height" => 750
+            ],
+            "id" => "45d10e_d6445cbc0df2428d9348c3a7639dbed8~mv2.jpg"
+          ]
+        ]
+        ],
+      "customTextFields" => [],
+      "manageVariants" => true,
+      "productOptions" =>  [
+        [
+          "optionType" => "color",
+          "name" => "Color",
+          "choices" =>  [
+             [
+              "value" => "#ffa500",
+              "description" => "Orange",
+              "inStock" => true,
+              "visible" => true,
+             ],
+            [
+              "value" => "#ffc0cb",
+              "description" => "Pink",
+              "inStock" => true,
+              "visible" => true,
+            ]
+          ]
+        ]
+            ],
+      "productPageUrl" =>  [
+        "base" => "https://www.dev-site-2x5664-1.wixdev-sites.org/",
+        "path" => "/product-page/now-product",
+      ],
+      "numericId" => "1681354961570000",
+      "inventoryItemId" => "e11c3939-1c00-0f28-99f6-63e2eaefa72b",
+      "discount" =>[
+        "type" => "NONE",
+        "value" => 0.0,
+      ],
+      "collectionIds" =>  [
+         "c4913c63-bed1-3420-8238-c66cbc5c97fa",
+         "00000000-000000-000000-000000000001",
+      ],
+      "variants" => [
+       [
+          "id" => "24caa640-7c95-46db-99fa-d3e7394e780f",
+          "choices" =>  [
+            "Color" => "Orange"
+          ],
+          "variant" =>  [
+            "priceData" => [
+              "currency" => "VND",
+              "price" => 2011.0,
+              "discountedPrice" => 2011.0,
+              "formatted" =>  [
+                "price" => "2.011₫",
+                "discountedPrice" => "2.011₫"
+              ]
+              ],
+            "convertedPriceData" =>  [
+              "currency" => "VND",
+              "price" => 2011.0,
+              "discountedPrice" => 2011.0,
+              "formatted" => [
+                "price" => "2.011₫",
+                "discountedPrice" => "2.011₫"
+              ]
+              ],
+            "weight" => 0.0,
+            "sku" => "",
+            "visible" => true
+              ],
+          "stock" =>[
+            "trackQuantity" => true,
+            "quantity" => 20,
+            "inStock" => true
+          ]
+          ],
+        [
+          "id" => "817089c1-0344-44f9-ae48-dffdd49b549d",
+          "choices" => [
+            "Color" => "Pink"
+          ],
+          "variant" => [
+            "priceData" => [
+              "currency" => "VND",
+              "price" => 2072.0,
+              "discountedPrice" => 2072.0,
+              "formatted" => [
+                "price" => "2.072₫",
+                "discountedPrice" => "2.072₫"
+              ]
+              ],
+            "convertedPriceData" => [
+              "currency" => "VND",
+              "price" => 2072.0,
+              "discountedPrice" => 2072.0,
+              "formatted" => [
+                "price" => "2.072₫",
+                "discountedPrice" => "2.072₫"
+              ]
+              ],
+            "weight" => 0.0,
+            "sku" => "",
+            "visible" => true,
+            ],
+          "stock" => [
+            "trackQuantity" => true,
+            "quantity" => 50,
+            "inStock" => true,
+          ]
+        ]
+          ],
+      "lastUpdated" => "2023-04-13T07:17:28.281Z",
+      "createdDate" => "2023-04-13T03:02:41.570Z",
+      "ribbon" => "Top Sellers",
+      "exportProductId" => "product_1ee3c6c6-e3ff-f0d7-6609-9c1d151058d4",
+    ]
+  ]
+  ?>
